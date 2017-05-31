9e203bbac91fdf8b12954e64b739eceaacfd1633
Alin <admin@alin.be>
2017-05-31 21:45:44 +0200

    Add extension .exe .bin

--- compile.sh
+++ compile.sh
@@ -8,3 +8,9 @@ for GOOS in darwin linux windows; do
     go build -o bin/goBloomsky-$GOOS-$GOARCH
   done
 done
+mv bin/goBloomsky-darwin-386 bin/goBloomsky-darwin-386.bin
+mv bin/goBloomsky-darwin-amd64 bin/goBloomsky-darwin-amd64.bin
+mv bin/goBloomsky-linux-386 bin/goBloomsky-linux-386.bin
+mv bin/goBloomsky-linux-amd64 bin/goBloomsky-linux-amd64.bin
+mv bin/goBloomsky-windows-386 bin/goBloomsky-windows-386.exe
+mv bin/goBloomsky-windows-amd64 bin/goBloomsky-windows-amd64.exe
