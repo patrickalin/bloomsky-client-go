a6fee55040f5edaac3538c23ffab8cd44ccda465
Alin <admin@alin.be>
2017-06-14 20:50:56 +0200

    Error in test

--- main_test.go
+++ main_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/nicksnyder/go-i18n/i18n"
 	mylog "github.com/patrickalin/GoMyLog"
 	"github.com/spf13/viper"
 )
@@ -17,8 +18,8 @@ func TestSomething(t *testing.T) {
 	}
 }
 func TestMain(m *testing.M) {
-	//i18n.MustLoadTranslationFile("lang/en-US.all.json")
-	//i18n.MustLoadTranslationFile("lang/fr.all.json")
+	i18n.MustLoadTranslationFile("lang/en-US.all.json")
+	i18n.MustLoadTranslationFile("lang/fr.all.json")
 	mylog.Init(mylog.ERROR)
 
 	os.Exit(m.Run())
