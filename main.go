a77f6891aaa8809d25a045220701dd3b94eb1d6f
Alin <admin@alin.be>
2017-06-22 15:12:10 +0200

    Change main position

--- main.go
+++ main.go
@@ -21,12 +21,7 @@ import (
 
 //configName name of the config file
 const configName = "config"
-
-//Version of the code
-var Version = "No Version Provided"
-var log = logrus.New()
-
-var body []byte
+const fileMock = "test-mock/mock.json"
 
 // Configuration is the structure of the config YAML file
 //use http://mervine.net/json2struct
@@ -51,83 +46,35 @@ type configuration struct {
 }
 
 var (
+	//Version of the code
+	Version = "No Version Provided"
+	//record the configuration parameter
 	config configuration
 
 	channels = make(map[string]chan bloomsky.BloomskyStructure)
 
 	debug = flag.String("debug", "", "Error=1, Warning=2, Info=3, Trace=4")
-	c     *httpServer
+	//logger
+	log              = logrus.New()
+	responseBloomsky []byte
 )
 
-// ReadConfig read config from config.json
-// with the package viper
-func readConfig(configName string) (err error) {
-	viper.SetConfigName(configName)
-	viper.AddConfigPath(".")
+func init() {
+	log.Formatter = new(logrus.JSONFormatter)
+	log.Formatter = new(logrus.TextFormatter)
 
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		return fmt.Errorf("%v", err)
+	file, err := os.OpenFile("bloomsky.log", os.O_CREATE|os.O_WRONLY, 0666)
+	if err == nil {
+		log.Out = file
+	} else {
+		log.Info("Failed to log to file, using default stderr")
 	}
-	dir = dir + "/" + configName
-	log.Infof("The config file loaded is : %s/%s", dir, configName)
-
-	if err := viper.ReadInConfig(); err != nil {
-		return err
-	}
-
-	//Todo find to simplify this section
-	config.bloomskyURL = viper.GetString("BloomskyURL")
-	config.bloomskyAccessToken = viper.GetString("BloomskyAccessToken")
-	config.influxDBDatabase = viper.GetString("InfluxDBDatabase")
-	config.influxDBPassword = viper.GetString("InfluxDBPassword")
-	config.influxDBServer = viper.GetString("InfluxDBServer")
-	config.influxDBServerPort = viper.GetString("InfluxDBServerPort")
-	config.influxDBUsername = viper.GetString("InfluxDBUsername")
-	config.consoleActivated = viper.GetBool("ConsoleActivated")
-	config.influxDBActivated = viper.GetBool("InfluxDBActivated")
-	config.refreshTimer = time.Duration(viper.GetInt("RefreshTimer")) * time.Second
-	config.hTTPActivated = viper.GetBool("HTTPActivated")
-	config.hTTPPort = viper.GetString("HTTPPort")
-	config.logLevel = viper.GetString("LogLevel")
-	config.mock = viper.GetBool("mock")
-	config.language = viper.GetString("language")
-	config.dev = viper.GetBool("dev")
-
-	if err := i18n.ParseTranslationFileBytes("lang/en-us.all.json", readTranslationResource("lang/en-us.all.json")); err != nil {
-		log.Fatalf("error read language file : %v", err)
-	}
-	if err := i18n.ParseTranslationFileBytes("lang/fr.all.json", readTranslationResource("lang/fr.all.json")); err != nil {
-		log.Fatalf("error read language file : %v", err)
-	}
-
-	config.translateFunc, err = i18n.Tfunc(config.language)
-	if err != nil {
-		log.Errorf("Problem with loading translate file, %v", err)
-	}
-
-	// Check if one value of the structure is empty
-	v := reflect.ValueOf(config)
-	values := make([]interface{}, v.NumField())
-	for i := 0; i < v.NumField(); i++ {
-		values[i] = v.Field(i)
-		//Todo
-		//v.Field(i).SetString(viper.GetString(v.Type().Field(i).Name))
-		if values[i] == "" {
-			return fmt.Errorf("Check if the key " + v.Type().Field(i).Name + " is present in the file " + dir)
-		}
-	}
-	if token := os.Getenv("bloomskyAccessToken"); token != "" {
-		config.bloomskyAccessToken = token
-	}
-	return nil
 }
 
 //go:generate ./command/bindata.sh
 //go:generate ./command/bindata-assetfs.sh
 
 func main() {
-
 	log.Debug("Create context")
 	myContext, cancel := context.WithCancel(context.Background())
 
@@ -162,7 +109,6 @@ func main() {
 	log.Level = level
 	log.Debugf("Level trace: %s", level)
 
-	//TODO pourquoi on redefini un deuxième context ?
 	ctxsch := context.Context(myContext)
 
 	if config.consoleActivated {
@@ -182,57 +128,44 @@ func main() {
 		c.listen(context.Background())
 
 	}
+
+	var httpServ *httpServer
 	if config.hTTPActivated {
 		var err error
 		channels["web"] = make(chan bloomsky.BloomskyStructure)
-		c, err = createWebServer(channels["web"], config.hTTPPort)
+		httpServ, err = createWebServer(channels["web"], config.hTTPPort)
 		if err != nil {
 			log.Fatalf("Error with initWebServer %v", err)
 		}
-		c.listen(context.Background())
+		httpServ.listen(context.Background())
 
 	}
 
 	if config.mock {
-		log.Warn("Mock activated !!!")
-		fileMock := "test-mock/mock.json"
-		if config.dev {
-			body, err = ioutil.ReadFile(fileMock)
-			if err != nil {
-				log.Fatalf("Error in reading the file %s Err:  %v", fileMock, err)
-			}
-		}
-		if !config.dev {
-			body, err = assembly.Asset(fileMock)
-			if err != nil {
-				log.Fatalf("Error in reading the file %s Err:  %v", fileMock, err)
-			}
-		}
-
+		responseBloomsky = readMockFile()
 	}
 
 	schedule(ctxsch)
 
 	<-myContext.Done()
-	//cancelsch()
-	if c.h != nil {
+	if httpServ.h != nil {
 		log.Debug("Shutting down ws")
-		c.h.Shutdown(myContext)
+		httpServ.h.Shutdown(myContext)
 	}
 
 	log.Debug("Terminated")
 }
 
-// The scheduler
+// The scheduler execute each time collect
 func schedule(myContext context.Context) {
 	ticker := time.NewTicker(config.refreshTimer)
 	log.Debug("Create scheduler")
 
-	collect(myContext)
+	collect()
 	for {
 		select {
 		case <-ticker.C:
-			collect(myContext)
+			collect()
 		case <-myContext.Done():
 			log.Debug("Stoping ticker")
 			ticker.Stop()
@@ -245,24 +178,86 @@ func schedule(myContext context.Context) {
 }
 
 //Principal function which one loops each Time Variable
-func collect(ctx context.Context) {
-
+func collect() {
 	log.Infof("Parse informations from API bloomsky each : %s", config.refreshTimer)
 
 	// get bloomsky JSON and parse information in bloomsky Go Structure
 	var mybloomsky bloomsky.BloomskyStructure
 	if config.mock {
-		mybloomsky = bloomsky.NewBloomskyFromBody(body)
-	}
-	if !config.mock {
+		mybloomsky = bloomsky.NewBloomskyFromBody(responseBloomsky)
+	} else {
 		log.Debug("Mock desactivated")
 		mybloomsky = bloomsky.NewBloomsky(config.bloomskyURL, config.bloomskyAccessToken, true)
 	}
 
+	//send message on each channels
 	for _, v := range channels {
 		v <- mybloomsky
 	}
+}
 
+// ReadConfig read config from config.json
+// with the package viper
+func readConfig(configName string) (err error) {
+	viper.SetConfigName(configName)
+	viper.AddConfigPath(".")
+
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+	dir = dir + "/" + configName
+	log.Infof("The config file loaded is : %s/%s", dir, configName)
+
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	//TODO#16 find to simplify this section
+	config.bloomskyURL = viper.GetString("BloomskyURL")
+	config.bloomskyAccessToken = viper.GetString("BloomskyAccessToken")
+	config.influxDBDatabase = viper.GetString("InfluxDBDatabase")
+	config.influxDBPassword = viper.GetString("InfluxDBPassword")
+	config.influxDBServer = viper.GetString("InfluxDBServer")
+	config.influxDBServerPort = viper.GetString("InfluxDBServerPort")
+	config.influxDBUsername = viper.GetString("InfluxDBUsername")
+	config.consoleActivated = viper.GetBool("ConsoleActivated")
+	config.influxDBActivated = viper.GetBool("InfluxDBActivated")
+	config.refreshTimer = time.Duration(viper.GetInt("RefreshTimer")) * time.Second
+	config.hTTPActivated = viper.GetBool("HTTPActivated")
+	config.hTTPPort = viper.GetString("HTTPPort")
+	config.logLevel = viper.GetString("LogLevel")
+	config.mock = viper.GetBool("mock")
+	config.language = viper.GetString("language")
+	config.dev = viper.GetBool("dev")
+
+	if err := i18n.ParseTranslationFileBytes("lang/en-us.all.json", readTranslationResource("lang/en-us.all.json")); err != nil {
+		log.Fatalf("error read language file : %v", err)
+	}
+	if err := i18n.ParseTranslationFileBytes("lang/fr.all.json", readTranslationResource("lang/fr.all.json")); err != nil {
+		log.Fatalf("error read language file : %v", err)
+	}
+
+	config.translateFunc, err = i18n.Tfunc(config.language)
+	if err != nil {
+		log.Errorf("Problem with loading translate file, %v", err)
+	}
+
+	// Check if one value of the structure is empty
+	v := reflect.ValueOf(config)
+	values := make([]interface{}, v.NumField())
+	for i := 0; i < v.NumField(); i++ {
+		values[i] = v.Field(i)
+		//TODO#16
+		//v.Field(i).SetString(viper.GetString(v.Type().Field(i).Name))
+		if values[i] == "" {
+			return fmt.Errorf("Check if the key " + v.Type().Field(i).Name + " is present in the file " + dir)
+		}
+	}
+	if token := os.Getenv("bloomskyAccessToken"); token != "" {
+		config.bloomskyAccessToken = token
+	}
+	return nil
 }
 
 //Read translation ressources from /lang or the assembly
@@ -281,16 +276,22 @@ func readTranslationResource(name string) []byte {
 	}
 	return b
 }
-func init() {
 
-	log.Formatter = new(logrus.JSONFormatter)
-	log.Formatter = new(logrus.TextFormatter) // default
+//If mock activated load the file mock and place it in the responseBloomsky
+func readMockFile() []byte {
+	fmt.Print("Mock activated !!!")
 
-	file, err := os.OpenFile("bloomsky.log", os.O_CREATE|os.O_WRONLY, 0666)
-	if err == nil {
-		log.Out = file
-	} else {
-		log.Info("Failed to log to file, using default stderr")
+	if config.dev {
+		mockFile, err := ioutil.ReadFile(fileMock)
+		if err != nil {
+			log.Fatalf("Error in reading the file %s Err:  %v", fileMock, err)
+		}
+		return mockFile
 	}
 
+	mockFile, err := assembly.Asset(fileMock)
+	if err != nil {
+		log.Fatalf("Error in reading the file %s Err:  %v", fileMock, err)
+	}
+	return mockFile
 }
