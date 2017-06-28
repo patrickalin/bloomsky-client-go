c1fc93b3115413ebe3f68594e00ffc4c63063e62
tixu <xavier.ernest.zebier@gmail.com>
2017-06-28 16:47:00 +0200

    added first ring

---
+++ ring_test.go
@@ -0,0 +1,28 @@
+package main
+
+// "fmt"
+import "testing"
+import "fmt"
+
+func TestSetsSize(t *testing.T) {
+	r := &Ring{}
+	r.SetCapacity(10)
+	if r.Capacity() != 10 {
+		t.Fatal("Size of ring was not 10", r.Capacity())
+	}
+}
+
+func TestSavesSomeData(t *testing.T) {
+	r := Ring{}
+	r.SetCapacity(7)
+	for i := 0; i <= 14; i++ {
+		r.Enqueue(i)
+	}
+	fmt.Println(r.head)
+	fmt.Println(r.tail)
+
+	for i := 0; i < 7; i++ {
+		fmt.Println(r.Dequeue().(int))
+
+	}
+}
