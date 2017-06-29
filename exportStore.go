e5746974011c4937674a13d2dfe6653c021999c0
tixu <xavier.ernest.zebier@gmail.com>
2017-06-29 14:29:01 +0200

    package for ring

--- exportStore.go
+++ exportStore.go
@@ -3,18 +3,24 @@ package main
 import (
 	"context"
 
+	"github.com/patrickalin/bloomsky-client-go/pkg/ring"
+
 	bloomsky "github.com/patrickalin/bloomsky-api-go"
 	"github.com/sirupsen/logrus"
 )
 
 type store struct {
 	in     chan bloomsky.Bloomsky
-	stores map[string]*Ring
+	stores map[string]*ring.Ring
 }
 
 type measure struct {
 	value float64
 }
+
+/**
+* Measure  represents a measure that has a GetValue
+ */
 
 func (m measure) Value() float64 {
 	return m.value
@@ -22,9 +28,9 @@ func (m measure) Value() float64 {
 
 //InitConsole listen on the chanel
 func createStore(messages chan bloomsky.Bloomsky) (store, error) {
-	stores := make(map[string]*Ring)
-	stores["temp"] = &Ring{}
-	stores["wind"] = &Ring{}
+	stores := make(map[string]*ring.Ring)
+	stores["temp"] = &ring.Ring{}
+	stores["wind"] = &ring.Ring{}
 	return store{in: messages, stores: stores}, nil
 
 }
@@ -48,7 +54,7 @@ func (c *store) listen(context context.Context) {
 
 }
 
-func (c *store) GetValues(name string) []Measure {
+func (c *store) GetValues(name string) []ring.Measure {
 	return c.stores[name].Values()
 
 }
