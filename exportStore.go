e7c801dbb71d2cb414e3891a8e2871ec1a305d76
Alin <admin@alin.be>
2017-07-28 22:42:24 +0200

    Improve chart

--- exportStore.go
+++ exportStore.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"strconv"
 	"time"
 
 	"github.com/patrickalin/bloomsky-client-go/pkg/ring"
@@ -36,7 +37,15 @@ func (m measure) Value() float64 {
 func createStore(messages chan bloomsky.Bloomsky) (store, error) {
 	stores := make(map[string]*ring.Ring)
 	stores["temperatureCelsius"] = &ring.Ring{}
+	stores["pressureHPa"] = &ring.Ring{}
 	stores["windGustkmh"] = &ring.Ring{}
+	stores["windSustainedSpeedkmh"] = &ring.Ring{}
+	stores["humidity"] = &ring.Ring{}
+	stores["rainDailyMm"] = &ring.Ring{}
+	stores["rainMm"] = &ring.Ring{}
+	stores["rainRate"] = &ring.Ring{}
+	stores["indexUV"] = &ring.Ring{}
+
 	return store{in: messages, stores: stores}, nil
 
 }
@@ -55,8 +64,20 @@ func (c *store) listen(context context.Context) {
 				log.WithFields(logrus.Fields{
 					"fct": "exportStore.listen",
 				}).Debug("Receive message")
+
 				c.stores["temperatureCelsius"].Enqueue(measure{time.Now(), msg.GetTemperatureCelsius()})
+				c.stores["pressureHPa"].Enqueue(measure{time.Now(), msg.GetPressureHPa()})
 				c.stores["windGustkmh"].Enqueue(measure{time.Now(), msg.GetWindGustkmh()})
+				c.stores["windSustainedSpeedkmh"].Enqueue(measure{time.Now(), msg.GetSustainedWindSpeedkmh()})
+				c.stores["humidity"].Enqueue(measure{time.Now(), msg.GetHumidity()})
+				c.stores["rainDailyMm"].Enqueue(measure{time.Now(), msg.GetRainDailyMm()})
+				c.stores["rainMm"].Enqueue(measure{time.Now(), msg.GetRainMm()})
+				c.stores["rainRate"].Enqueue(measure{time.Now(), msg.GetRainRateMm()})
+
+				i, err := strconv.ParseFloat(msg.GetIndexUV(), 64)
+				checkErr(err, funcName(), "impossible to convert string to int")
+				c.stores["indexUV"].Enqueue(measure{time.Now(), i})
+
 			case <-context.Done():
 				return
 			}
