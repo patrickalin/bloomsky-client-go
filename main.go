b927e9e3122733b538c5c64b2fa37d43979a975c
Alin <admin@alin.be>
2017-08-03 19:16:02 +0200

    new page parameter + find the bug parse ....

--- main.go
+++ main.go
@@ -37,7 +37,6 @@ const (
 type configuration struct {
 	consoleActivated    bool
 	hTTPActivated       bool
-	historyActivated    bool
 	hTTPPort            string
 	hTTPSPort           string
 	influxDBActivated   bool
@@ -144,7 +143,7 @@ func startServer(mycontext context.Context, config configuration) stopServer {
 
 		channels["web"] = make(chan bloomsky.Bloomsky)
 
-		httpServ, err = createWebServer(channels["web"], config.hTTPPort, config.hTTPSPort, translateFunc, config.dev, store, config.wss)
+		httpServ, err = createWebServer(channels["web"], config.hTTPPort, config.hTTPSPort, translateFunc, config.dev, store, config.wss, config)
 		checkErr(err, funcName(), "Error with initWebServer")
 		ctxthttp, cancelhttp := context.WithCancel(mycontext)
 		defer cancelhttp()
@@ -243,21 +242,32 @@ func readConfig(configName string, options ...validation) configuration {
 
 	pflag.String("main.bloomsky.token", "rrrrr", "yourtoken")
 	pflag.Bool("main.dev", false, "developpement mode")
-	pflag.Bool("main.mock", false, "use mock  mode")
+	pflag.Bool("main.mock", false, "use mock mode")
 
-	fmt.Println("ici")
+	p.VisitAll(func(flag *pflag.Flag) {
+		fmt.Printf("ICI %v : %v \n", flag.Name, flag.Value)
+	})
 
 	pflag.Parse()
 
-	fmt.Println("la")
+	p.VisitAll(func(flag *pflag.Flag) {
+		fmt.Printf("LA %v : %v \n", flag.Name, flag.Value)
+	})
 
 	//viper.BindFlagValue("main.bloomsky.token")
 	viper.SetConfigType("yaml")
 	viper.SetConfigName(configName)
 	viper.AddConfigPath(".")
 	viper.AddConfigPath("test")
+
+	p := pflag.CommandLine
+
 	err := viper.BindPFlags(pflag.CommandLine)
 	checkErr(err, funcName(), "Error with bindPFlags")
+
+	conf.mock = viper.GetBool("main.mock")
+	fmt.Println(os.Args[2])
+	fmt.Println(viper.GetBool("main.mock"))
 
 	viper.SetDefault("main.language", "en-us")
 	viper.SetDefault("main.RefreshTimer", 60)
@@ -277,7 +287,6 @@ func readConfig(configName string, options ...validation) configuration {
 	}
 
 	conf.mock = viper.GetBool("main.mock")
-
 	conf.dev = viper.GetBool("main.dev")
 
 	//TODO#16 find to simplify this section
@@ -289,7 +298,6 @@ func readConfig(configName string, options ...validation) configuration {
 	conf.language = main.GetString("language")
 	conf.logLevel = main.GetString("log.level")
 	conf.wss = main.GetBool("wss")
-	conf.historyActivated = viper.GetBool("historyActivated")
 	conf.refreshTimer = time.Duration(main.GetInt("refreshTimer")) * time.Second
 
 	web := viper.Sub("outputs.web")
